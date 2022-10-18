d4ac695e13c9e7b6a9813c9b145557b7967cb065
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-10-18 17:56:06 +0530

    Update stats.c
    Added new function for average, min and max

--- stats.c
+++ stats.c
@@ -2,9 +2,45 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.average = GetAvgerageValue(numberset,setlength);
+    s.min = GetMinimumValue(numberset,setlength);
+    s.max = GetMaximumValue(numberset,setlength);
+}
+
+float GetMinimumValue(const float* numberset,int setlength)
+{ 
+    float min = 100000;
+    for(int i=0;i<setlength;i++)
+    {
+	    if(numberset[i] < min)
+	    {
+	    min = numberset[i];
+	    }
+    }
+return min;
+}
+
+float GetMaximumValue(const float* numberset,int setlength)
+{
+    float max = -5000;
+    for(int i=0;i<setlength;i++)
+    {
+	    if(numberset[i] > max)
+	    {
+	    max = numberset[i];
+	    }
+    }
+return max;
+}
+
+float GetAvgerageValue(const float* numberset,int setlength)
+{
+   float avg =0;
+    for(int i=0;i<setlength;i++)
+    {
+	    avg+=numberset[i];
+    }
+return (float)(avg/setlength);
 }
 
 int emailAlertCallCount = 0;
