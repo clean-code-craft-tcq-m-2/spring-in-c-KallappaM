8dbc22e67e9bb93410400e85a4ccac40f047c132
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-10-18 18:23:53 +0530

    Update stats.h
    updated name changes

--- stats.h
+++ stats.h
@@ -6,7 +6,7 @@ struct Stats {
 };
 struct Stats compute_statistics(const float* numberset, int setlength);
 float GetMaximumValue(const float* numberset,int setlength);
-float GetAverageValue(const float* numberset,int setlength);
+float GetAvgerageValue(const float* numberset,int setlength);
 float GetMinimumValue(const float* numberset,int setlength);
 
 typedef void (*alerter_funcptr)();
